1ad0efc8de99591e00f0baa7997fedd1799fdb53
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-07 12:15:06 +0300

    Update Advanced Arrays-distinctArray-Task-02.js

--- Advanced Arrays/Advanced Arrays-distinctArray-Task-02.js
+++ Advanced Arrays/Advanced Arrays-distinctArray-Task-02.js
@@ -1,3 +1,9 @@
+// You will be given an array of integer numbers on the first line of the input.
+
+// Remove all repeating elements from the array.
+
+// Print the result elements separated by a single space.
+
 function distinctArray(array) {
     let newArr = []
 
