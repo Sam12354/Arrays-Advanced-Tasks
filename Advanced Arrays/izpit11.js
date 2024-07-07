e5443935c286f7f70e73ed79acd8845a9d746334
Sam12354 <161862599+Sam12354@users.noreply.github.com>
2024-07-07 12:13:22 +0300

    Advanced Arrays-distinctArray-Task-02.js

---
+++ Advanced Arrays/izpit11.js
@@ -0,0 +1,16 @@
+function distinctArray(array) {
+    let newArr = []
+
+    for(let i = 0; i < array.length; i++){
+        let curNum = array[i]
+
+        if(!newArr.includes(curNum)){
+            newArr.push(curNum)
+        }
+    }
+    console.log(newArr.join(' '));
+}
+distinctArray
+([1, 2, 3, 4])
+// ([7, 8, 9, 7, 2, 3, 4, 1, 2])
+// ([20, 8, 12, 13, 4, 4, 8, 5])
